9ac2fa38e35f96176f5bd0f4c92655e910609f8d
Muzzaiyyan Hussain <muzzaiyyan.web3@gmail.com>
2026-02-17 18:15:50 +0530

    header

---
+++ modules/home/components/navbar.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image"
+import Link from "next/link"
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
+import { Button } from "@/components/ui/button"
+
+const Navbar = () => {
+    return (
+        <nav className='p-4 bg-transparent fixed top-0 left-0 right-0 z-50 transition-all duration-200 border-b border-transparent'>
+            <div className='max-w-5xl mx-auto w-full flex justify-between items-center'>
+                <Link href={"/"} className="flex items-center gap-2">
+                    <Image src={"/logo.svg"} width={32} height={32} alt="Vibe" className="shrink-0 invert dark:invert-0" />
+                </Link>
+                <SignedOut>
+                    <div className="flex gap-2">
+                        <SignUpButton>
+                            <Button variant={"outline"} size={"sm"}>Sign Up</Button>
+                        </SignUpButton>
+
+                        <SignInButton>
+                            <Button size={"sm"}>Sign In</Button>
+                        </SignInButton>
+
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                    </div>
+                </SignedOut>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
